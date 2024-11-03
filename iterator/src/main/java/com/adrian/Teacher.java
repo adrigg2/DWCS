5ba1175d1d0117d64fc00544a8fe4d72011f36fa
adrigg2 <adriangarcia2.agg@gmail.com>
2024-11-03 01:02:13 +0100

    03-11-2024

--- iterator/src/main/java/com/adrian/Teacher.java
+++ iterator/src/main/java/com/adrian/Teacher.java
@@ -1,15 +1,18 @@
 package com.adrian;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.NoSuchElementException;
+import java.util.TreeSet;
 
 // Class to represent a teacher. Can be iterated to check their students one by one
 public class Teacher extends Person implements Iterable<Student> {
-    private LinkedHashSet<Student> students;
+    private TreeSet<Student> students;
 
     public Teacher(int id, String name) {
         super(id, name);
-        students = new LinkedHashSet<>();
+        students = new TreeSet<>(new StudentComparator());
     }
 
     public boolean addStudent(Student student) {
@@ -24,20 +27,50 @@ public class Teacher extends Person implements Iterable<Student> {
 
     // Class that creates the iterator for the students
     private class StudentIter implements Iterator<Student> {
-        private Iterator<Student> iterator;
+        private int index = 0;
+        private ArrayList<Student> iterator;
 
         public StudentIter() {
-            iterator = students.iterator();
+            iterator = new ArrayList<>(students);
         }
 
         @Override
         public boolean hasNext() {
-            return iterator.hasNext();
+            return index < iterator.size();
         }
 
         @Override
         public Student next() {
-            return iterator.next();
-        }        
+            if (!hasNext()) {
+                throw new NoSuchElementException("There are no more elements in the list");
+            }
+            // Returns the next element and increments then increments the index
+            return iterator.get(index++);
+        }
+
+        public boolean hasPrevious() {
+            return index > 0;
+        }
+
+        public Student previous() {
+            if (!hasPrevious()) {
+                throw new NoSuchElementException("There are no more elements in the list");
+            }
+            return iterator.get(index--);
+        }
+    }
+
+    // Class that defines how to compare Students.
+    private class StudentComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student o1, Student o2) {
+            if (o1.getGrade() > o2.getGrade()) {
+                return 1;
+            } else if (o1.getGrade() < o2.getGrade()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
